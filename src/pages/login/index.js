8d9cca80942a2cea681898072be2e08ef0d7cf64
Michael da silva ribeiro <108340186+DevMike3@users.noreply.github.com>
2022-11-23 15:14:02 -0300

    .

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -22,7 +22,7 @@ export default function Login() {
       <div className="w-full h-full justify-center items-center flex ">
         <h1 className="font-bold  text-4xl">
           <Typewriter onInit={(typewriter) => {
-            typewriter.typeString('Holla, seja bem-vindo a o gerenciamento de ondominio da suam.')
+            typewriter.typeString('Seja bem-vindo a o gerenciamento de condominio da suam.')
               .pauseFor(2500)
               .start();
           }}
